4961a4b1866d0be05b0d126e13f365d97bcfe07b
DymNomZ <dymierborgonia@gmail.com>
2025-03-23 20:46:03 +0800

    Added Create and Edit Task Activities

---
+++ app/src/main/java/com/dymnomz/task_forge/CreateTaskActivity.kt
@@ -0,0 +1,10 @@
+package com.dymnomz.task_forge
+
+import android.app.Activity
+import android.os.Bundle
+
+class CreateTaskActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/dymnomz/task_forge/EditTaskActivity.kt
@@ -0,0 +1,10 @@
+package com.dymnomz.task_forge
+
+import android.app.Activity
+import android.os.Bundle
+
+class EditTaskActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
